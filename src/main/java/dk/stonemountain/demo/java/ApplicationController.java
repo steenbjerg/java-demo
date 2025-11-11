bacc0c13f758591a8caa15d6841c04769f89693e
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2025-11-11 16:54:03 +0100

    created an fxml application

--- src/main/java/dk/stonemountain/demo/java/ApplicationController.java
+++ src/main/java/dk/stonemountain/demo/java/ApplicationController.java
@@ -1,51 +1,64 @@
 package dk.stonemountain.demo.java;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ApplicationController {
-    @FXML
-    private TableView<Car> carTable; // The TableView from FXML
-    
-    @FXML
-    private TableColumn<Car, String> brandColumn;
-    
-    @FXML
-    private TableColumn<Car, String> nameColumn;
-    
-    @FXML
-    private TableColumn<Car, String> typeColumn;
+    @FXML private TableView<Car> carTable;
+    @FXML private TableColumn<Car, String> brandColumn;
+    @FXML private TableColumn<Car, String> nameColumn;
+    @FXML private TableColumn<Car, String> typeColumn;
+    @FXML private TextField entityBrand;
+    @FXML private TextField entityName;
+    @FXML private ChoiceBox<Car.Type> entityType;
     
     ObservableList<Car> carList = FXCollections.observableArrayList();
+    Car editableCar = new Car();
     
     @FXML
     private void initialize() {
-        // Initialize the table columns
+        // Initialize the table and the columns
         brandColumn.setCellValueFactory(cellData -> cellData.getValue().brandProperty());
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-        typeColumn.setCellValueFactory(cellData -> cellData.getValue().typeProperty());
-        
-        // Initialize the table
+        typeColumn.setCellValueFactory(cellData -> Bindings.createStringBinding(() -> cellData.getValue().getType().name(), cellData.getValue().typeProperty()));
+
         carTable.setItems(carList);
+
+        // Initialize the form
+        entityType.setItems(FXCollections.observableArrayList(Car.Type.values()));
+        entityType.setConverter(new Car.Type.DisplayConverter());
+        entityType.setValue(Car.Type.Sedan);
+        
+        entityBrand.textProperty().bindBidirectional(editableCar.brandProperty());
+        entityName.textProperty().bindBidirectional(editableCar.nameProperty());
+
+        entityType.valueProperty().bindBidirectional(editableCar.typeProperty());
+
+        carTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            editableCar.update(newSelection);
+        });
     }
 
     @FXML
     private void doAddRows() {
-        carList.add(new Car("Toyota", "Corolla", "Sedan"));
-        carList.add(new Car("Honda", "Civic", "Sedan"));
-        carList.add(new Car("Ford", "Mustang", "Coupe"));
-        carList.add(new Car("Chevrolet", "Camaro", "Coupe"));
-        carList.add(new Car("Dodge", "Charger", "Coupe"));
-        carList.add(new Car("Chevrolet", "Camaro", "Coupe"));
-        carList.add(new Car("Chevrolet", "Camaro", "Coupe"));
-        carList.add(new Car("Chevrolet", "Camaro", "Coupe"));
-        carList.add(new Car("Porsche", "911", "Coupe"));
-        carList.add(new Car("Porsche", "911", "Coupe"));
-        carList.add(new Car("Porsche", "911", "Coupe"));
+        carList.add(new Car("Toyota", "Corolla", Car.Type.Sedan));
+        carList.add(new Car("Honda", "Civic", Car.Type.Sedan));
+        carList.add(new Car("Ford", "Mustang", Car.Type.Coupe));
+        carList.add(new Car("Chevrolet", "Camaro", Car.Type.Coupe));
+        carList.add(new Car("Dodge", "Charger", Car.Type.Coupe));
+        carList.add(new Car("Chevrolet", "Camaro", Car.Type.Coupe));
+        carList.add(new Car("Chevrolet", "Camaro", Car.Type.Coupe));
+        carList.add(new Car("Chevrolet", "Camaro", Car.Type.Coupe));
+        carList.add(new Car("Porsche", "911", Car.Type.Coupe));
+        carList.add(new Car("Porsche", "911", Car.Type.Coupe));
+        carList.add(new Car("Porsche", "911", Car.Type.Coupe));
     }
 
     @FXML
@@ -59,5 +72,16 @@ public class ApplicationController {
     private void doAbout() {
         // Implementation for About action
         System.out.println("About action triggered");
+    }
+
+    @FXML
+    private void doCancel() {
+        editableCar.update(carTable.getSelectionModel().getSelectedItem());
+    }
+
+    @FXML
+    private void doSave() {
+        Car car = carTable.getSelectionModel().getSelectedItem(); 
+        car.update(editableCar);
     }
 }
--- src/main/java/dk/stonemountain/demo/java/Car.java
+++ src/main/java/dk/stonemountain/demo/java/Car.java
@@ -1,21 +1,56 @@
 package dk.stonemountain.demo.java;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.util.StringConverter;
 
 public class Car {
+    public enum Type {
+        Sedan,
+        Coupe,
+        Hatchback,
+        Convertible;
+
+        public static class DisplayConverter extends StringConverter<Type> {
+            @Override
+            public String toString(Type type) {
+                if (type == null) return null;
+                return type.name().toLowerCase();
+            }
+
+            @Override
+            public Type fromString(String s) {
+                return null;
+            }
+        }
+    }
+
     private StringProperty brand = new SimpleStringProperty();   
     private StringProperty name = new SimpleStringProperty();
-    private StringProperty type = new SimpleStringProperty();
+    private ObjectProperty<Type> type = new SimpleObjectProperty<>();
     
 
     public Car() {
     }
     
-    public Car(String brand, String name, String type) {
+    public Car(String brand, String name, Type type) {
         this.brand.set(brand);
         this.name.set(name);
         this.type.set(type);
+    }
+
+    public Car(Car other) {
+        this.brand.set(other.brand.get());
+        this.name.set(other.name.get());
+        this.type.set(other.type.get());
+    }
+
+    public void clear() {
+        this.brand.set("");
+        this.name.set("");
+        this.type.set(Type.Sedan);
     }
 
     public final StringProperty brandProperty() {
@@ -42,21 +77,27 @@ public class Car {
         nameProperty().set(value);
     }
     
-    public final StringProperty typeProperty() {
+    public final ObjectProperty<Type> typeProperty() {
         return type;
     }
     
-    public final String getType() {
+    public final Type getType() {
         return typeProperty().get();
     }
     
-    public final void setType(String value) {
+    public final void setType(Type value) {
         typeProperty().set(value);
     }
     
     @Override
     public String toString() {
         return "Car{" + "brand=" + getBrand() + ", name=" + getName() + ", type=" + getType() + '}';
+    }
+
+    public void update(Car other) {
+        this.brand.set(other.brand.get());
+        this.name.set(other.name.get());
+        this.type.set(other.type.get());
     }
 
 }
